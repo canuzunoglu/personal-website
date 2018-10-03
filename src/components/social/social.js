506490120cc1e42353e56cb1c2aa23c7025836b5
Can Uzunoglu <bcanuzunoglu@gmail.com>
2018-10-03 10:20:28 +0300

    added github link

--- src/components/social/social.js
+++ src/components/social/social.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaTwitter, FaInstagram, FaLinkedinIn } from 'react-icons/fa';
+import { FaTwitter, FaInstagram, FaLinkedinIn, FaGithub } from 'react-icons/fa';
 import { Container, Icon } from './social.css';
 
 const Social = () => (
@@ -7,6 +7,15 @@ const Social = () => (
     <Icon
       target="_blank"
       rel="noreferrer"
+      title="Can Uzunoglu - LinkedIn"
+      href="https://www.linkedin.com/in/can-uzuno%C4%9Flu-163bb223"
+    >
+      <FaLinkedinIn size={30} />
+    </Icon>
+    <Icon
+      target="_blank"
+      rel="noreferrer"
+      title="Can Uzunoglu - Twitter"
       href="https://twitter.com/canuzunoglu"
     >
       <FaTwitter size={30} />
@@ -14,6 +23,7 @@ const Social = () => (
     <Icon
       target="_blank"
       rel="noreferrer"
+      title="Can Uzunoglu - Instagram"
       href="https://www.instagram.com/canuzunoglu"
     >
       <FaInstagram size={30} />
@@ -21,9 +31,10 @@ const Social = () => (
     <Icon
       target="_blank"
       rel="noreferrer"
-      href="https://www.linkedin.com/in/can-uzuno%C4%9Flu-163bb223"
+      title="Can Uzunoglu - Github"
+      href="https://www.github.com/canuzunoglu"
     >
-      <FaLinkedinIn size={30} />
+      <FaGithub size={30} />
     </Icon>
   </Container>
 );
